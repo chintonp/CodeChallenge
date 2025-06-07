10c0a12d73eaaa7d05abcb23147d1f21be0e2a88
Chinton Petersen <chintonpetersen@gmail.com>
2025-06-07 18:34:49 -0300

    JSON step 2

--- json/chjson.c
+++ json/chjson.c
@@ -9,11 +9,50 @@
 
 #define JSON_VALID 0
 #define JSON_INVALID 1
+#define STRING_VALID 2
+#define STRING_INVALID 3
 
 // Macro
 
-#define CHAR_READ c = fgetc(f); \
-	if (c == EOF) goto pj_end;
+#define CHAR_READ(ERROR) c = fgetc(f); \
+	if (c == EOF) goto ERROR;
+
+int parseString(FILE *f) 
+{
+	char c;
+	
+ps_st0:
+    CHAR_READ (ps_end)
+	if (c == '\\') goto ps_st1;
+	if (c == '"') return STRING_VALID;
+	goto ps_st0;
+	
+ps_st1:
+	CHAR_READ (ps_end)
+	switch (c) {
+		case '"':
+		case '\\':
+		case '/':
+		case 'b':
+		case 'f':
+		case 'n':
+		case 'r':
+		case 't':
+			goto ps_st0;
+		case 'u': 
+			break;
+		default:
+			goto ps_end;
+	}
+	for (int i = 0; i < 4; i++) {
+		CHAR_READ (ps_end);
+		if (!isxdigit(c)) goto ps_end;
+	}
+	goto ps_st0;
+	
+ps_end:
+	return STRING_INVALID;
+}
 
 int parseJson(char *fileName)
 {
@@ -28,20 +67,48 @@ int parseJson(char *fileName)
 	bool valid = false;
 
 pj_st0:	
-	CHAR_READ
+	CHAR_READ (pj_end)
 	if (isspace(c)) goto pj_st0;
 	if (c != '{') goto pj_end;
 	
 pj_st1:
-	CHAR_READ
+	CHAR_READ (pj_end)
 	if (isspace(c)) goto pj_st1;
+	if (c == '"') goto pj_st3;
 	if ( c != '}') goto pj_end;
 	valid = true;
 	
-pj_st2:
-	CHAR_READ
+pj_st2:  
+	CHAR_READ (pj_end)
 	if (isspace(c)) goto pj_st2;
 	valid = false;
+	goto pj_end;
+	
+pj_st3: 	
+	if (parseString(f) == STRING_INVALID) goto pj_end;
+	CHAR_READ (pj_end)
+	if (isspace(c)) goto pj_st3;
+	if (c != ':') goto pj_end;
+	
+pj_st4:
+	CHAR_READ (pj_end);
+	if (isspace(c)) goto pj_st4;
+	if (c != '"') goto pj_end;
+	
+	if (parseString(f) == STRING_INVALID) goto pj_end;
+	
+pj_st5: 
+	CHAR_READ (pj_end);
+	if (isspace(c)) goto pj_st5;
+	if (c == ',') goto pj_st6;
+	if ( c != '}') goto pj_end;
+	valid = true;
+	goto pj_st2;
+	
+pj_st6:
+	CHAR_READ (pj_end);
+	if (isspace(c)) goto pj_st6;
+	if (c == '"') goto pj_st3;
 
 pj_end:
 	fclose(f);
